10b53dfec58afad56c4d905867d7a7fa20222c31
wuchunhua <wuchunhua@zj.tech>
2023-05-17 10:36:30 +0800

    v1.8.0 theme update

--- init.lua
+++ init.lua
@@ -33,6 +33,6 @@ config.mappings = require "user.mappings"
 config.lazy = require "user.lazy"
 config.plugins = require "user.plugins"
 -- Set colorscheme to use
-config.colorscheme = "iceberg"
+config.colorscheme = "catppuccin"
 
 return config
